929b033d519b56cad5adef156f0c2ba2497009e6
le blond erwan <erwanleblond@gmail.com>
2026-07-12 10:18:54 +0200

    clean output

--- IAC/terraform/outputs.tf
+++ IAC/terraform/outputs.tf
@@ -18,11 +18,6 @@ output "docs_url" {
   value       = "http://${aws_eip.rag_server.public_ip}:${var.api_port}/docs"
 }
 
-output "client_url" {
-  description = "Client url"
-  value       = "http://${aws_eip.rag_server.public_ip}:8080/client_demo.html"
-}
-
 output "ssh_command" {
   description = "SSH command to connect to the instance"
   value       = "ssh -i ${var.ssh_private_key_path} ubuntu@${aws_eip.rag_server.public_ip}"
