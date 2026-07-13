dd70cf792d183467584666f12355ea94cf7603fb
le blond erwan <erwanleblond@gmail.com>
2026-07-13 16:58:25 +0200

    add grafana url

--- IAC/terraform/outputs.tf
+++ IAC/terraform/outputs.tf
@@ -18,6 +18,12 @@ output "docs_url" {
   value       = "http://${aws_eip.rag_server.public_ip}:${var.api_port}/docs"
 }
 
+output "grafana_url" {
+  description = "grafana"
+  value       = "http://${aws_eip.rag_server.public_ip}:3000"
+}
+
+
 output "ssh_command" {
   description = "SSH command to connect to the instance"
   value       = "ssh -i ${var.ssh_private_key_path} ubuntu@${aws_eip.rag_server.public_ip}"
